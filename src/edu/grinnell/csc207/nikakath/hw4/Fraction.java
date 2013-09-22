fac31dcda45bbc4dbc8378a486f2363dfcc63158
KittNika <kittnika@gmail.com>
2013-09-22 14:32:05 -0500

    Fraction Class

---
+++ src/edu/grinnell/csc207/nikakath/hw4/Fraction.java
@@ -0,0 +1,22 @@
+package edu.grinnell.csc207.nikakath.hw4;
+
+public class Fraction {
+
+	public static void main(String[] args) {
+	
+	}
+
+	private int numerator;
+	private int denominator;
+	
+	public Fraction(int num, int den) {
+		this.numerator = num;
+		this.denominator = den;
+	}
+	
+	public Fraction(int i) {
+		this.numerator = i;
+		this.denominator = 1;
+	}
+	
+}
