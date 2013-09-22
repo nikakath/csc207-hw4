985d70e089a0801b6a276998d39e2ad72147ff5f
KittNika <kittnika@gmail.com>
2013-09-22 14:24:44 -0500

    Starting Out

---
+++ src/edu/grinnell/csc207/nikakath/hw4/Fractions.java
@@ -0,0 +1,13 @@
+package edu.grinnell.csc207.nikakath.hw4;
+
+public class Fractions {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
