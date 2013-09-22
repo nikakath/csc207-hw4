4e626bfc399351f1d354794295be6db99021a368
KittNika <kittnika@gmail.com>
2013-09-22 14:47:47 -0500

    Constructors

--- src/edu/grinnell/csc207/nikakath/hw4/Fraction.java
+++ src/edu/grinnell/csc207/nikakath/hw4/Fraction.java
@@ -1,22 +1,58 @@
 package edu.grinnell.csc207.nikakath.hw4;
 
+import java.math.BigInteger;
+
 public class Fraction {
 
 	public static void main(String[] args) {
 	
 	}
 
-	private int numerator;
-	private int denominator;
+	private BigInteger numerator;
+	private BigInteger denominator;
 	
 	public Fraction(int num, int den) {
+		this.numerator = BigInteger.valueOf((long) num);
+		this.denominator = BigInteger.valueOf((long) den);
+	}
+	
+	public Fraction(int num) {
+		this.numerator = BigInteger.valueOf((long) num);
+		this.denominator = BigInteger.ONE;
+	}
+	
+	public Fraction(BigInteger num, BigInteger den) {
 		this.numerator = num;
 		this.denominator = den;
 	}
 	
-	public Fraction(int i) {
-		this.numerator = i;
-		this.denominator = 1;
+	public Fraction(BigInteger num) {
+		this.numerator = num;
+		this.denominator = BigInteger.ONE;
+	}
+	
+	public Fraction(long num, long den) {
+		this.numerator = BigInteger.valueOf(num);
+		this.denominator = BigInteger.valueOf(den);
+	}
+	
+	public Fraction(long num) {
+		this.numerator = BigInteger.valueOf(num);
+		this.denominator = BigInteger.ONE;
+	}
+	
+	public Fraction(double num, double den) {
+		this.numerator = BigInteger.valueOf((long) num);
+		this.denominator = BigInteger.valueOf((long) den);
+	}
+	
+	public Fraction(double num) {
+		this.numerator = BigInteger.valueOf((long) num);
+		this.denominator = BigInteger.ONE;
+	}
+	
+	public Fraction(String fraction) {
+		
 	}
 	
 }
